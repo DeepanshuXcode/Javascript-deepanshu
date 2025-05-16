4685f1d0d9b083db8250686ea5042dafb9ae6ff4
DeepanshuXcode <deepanshusingh0708@gmail.com>
2025-05-16 00:03:05 +0000

    Datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"
+/*
+why we use "use strict" because older version of javascript does not have 
+many things.Later that things were use in code so that's why we use "use strict".
+TREAT ALL JS CODE AS NEWER VERSION
+*/
+
+//alert( 3 + 3 ) // we are using node.js , not browser
+
+console.log(3  
+    + 
+    3)  // We should not use this type of practises bcz its decreses readability.
+
+let number = "Deepanshu"
+let age = 19
+let isLoggedIn = true
+
+// number => 2 to power 53
+// bigint
+// string =>"" or ''
+// boolean => true/false
+// null =>standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof "DEEP");
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
