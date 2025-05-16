ef1d364c34e4b2aa647025f33938643d8652ccca
DeepanshuXcode <deepanshusingh0708@gmail.com>
2025-05-16 21:26:35 +0000

    conversionOperation

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,56 @@
+// let score = 33
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let newnumber = false
+// console.log(typeof newnumber);
+
+// let insistnumber = Number(newnumber)
+// console.log(typeof insistnumber);
+// console.log(insistnumber)
+
+
+//number to string
+
+// let somenumber = 55
+// let stringNumber = String(somenumber)
+
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+
+
+//*************OPERATIONS************ //
+
+let value = 5
+let negvalue = -value
+//console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " DEEP"
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2)  //12 
+// console.log(1 + "2")  //12
+// console.log("1" + "2")  //12
+// console.log("1" + 2 + 2)  //122
+// console.log(1 + 2 + "2")  //32
+
+
+//prefix and postfix
+
+let gamecounter = 100
+gamecounter++  // or ++gamecounter output : 101 , basically dono condition me output same hai but hmesa nhi hota h
+
+console.log(gamecounter);
