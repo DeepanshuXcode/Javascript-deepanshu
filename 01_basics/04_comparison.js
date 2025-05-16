cc571ec1450d2bec3f2fb855606fdf7ce72ca6ca
DeepanshuXcode <deepanshusingh0708@gmail.com>
2025-05-16 23:15:05 +0000

    Comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,41 @@
+// console.log(2 > 1); //true
+// console.log(2 >= 1); //true
+// console.log(2 < 1);  //false
+// console.log(2 == 1); //false
+// console.log(2 != 1);  //true
+// basic conversion type of comparison 
+
+
+// console.log("2" > 1);  //true
+// console.log("02" > 1);  //true
+
+// Inn dono m output shi aaya hai prr hme hmesa ye ensure krna hai ki hmare datatype same ho.
+//different datatype use ho rha hai toh confusion hoga.
+
+
+/*
+console.log(null > 0);  //false
+console.log(null== 0);  //false
+console.log(null >= 0);  //true
+
+console.log(undefined == 0);  //false
+console.log(undefined > 0);  //false
+console.log(undefined < 0);  //false
+
+humko hmesa aisa code yaa aise practises ko avoid krna hai kyuki ye confusion laate hai code me.
+humko hmesa clean code likhna hai kyuki sbse jayda uski value hai.
+
+*/
+
+
+// === (Strict check)
+console.log("2" == 2); // true
+// ye == me true hoga kyuki yha p datatype check nhi ho rha hai.
+
+console.log("2" === 2); //false
+// yha false aaya kyuki strict check hua mtlb datatype bhi check hua.
+
+
+
+
+
