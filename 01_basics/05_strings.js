17cfa291772d09da75bc2268c4ebd518dddd8253
DeepanshuXcode <deepanshusingh0708@gmail.com>
2025-05-18 19:57:44 +0000

    String

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,56 @@
+const name = "DEEP"
+const age = 20
+
+//console.log(name + age + "sahil" );
+
+//console.log(`Hello my name is ${name} and my age is ${age}`);
+// This is new way syntax for declaring string.
+
+
+const gameName = new String("Deepanshu")
+// console.log(gameName[0]);
+// console.log(gameName.small());
+// console.log(gameName.bold());
+// console.log(gameName.charAt(3));
+
+
+const newString = gameName.substring(0,6)
+//console.log(newString);
+
+const anotherString = gameName.slice(-8,4)
+//console.log(anotherString);
+
+
+const newstringOne = "       DEEPANSHU    "
+//console.log(newstringOne.trim());
+//console.log(newstringOne);
+//Trim tb use krte hai jb hme extra spaces htane hote hai
+
+  
+const url = "https://www.youtube.com/watch?v=fozwNnFunlo&t=442s"
+//console.log(url.replace('watch?v','deepanshu'));
+//replace 
+
+
+
+//console.log(url.includes('khy'));
+//finding
+
+const newSplit = "Deepika-Raj-Kohli"
+console.log(newSplit.split('-'));
+//Hum kisi bhi datatype ko array ko convert kr skte hai by split fuction or bracket me woh dete hai jisse hme split krna hota e.g. ('-')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
