6752bf731319bb5655b97edead3cc83088aa2839
DeepanshuXcode <deepanshusingh0708@gmail.com>
2025-05-19 21:56:33 +0000

    Array-1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,70 @@
+const myArr = [0,1,2,8,4,5]
+
+const myHeros = ["thor","Krish","Don"]
+
+
+
+const myArr2 = new Array(2,4,7,4,6)
+//we can also declare array like this
+
+//console.log(myArr[3]);
+
+
+
+//Array method
+
+myArr.push(9)
+myArr.push(6)  // last me value add krta hai
+
+myArr.pop()  //last value hta deta hai
+
+myArr.unshift(7) //starting me value add krta h
+
+myArr.shift()  //starting ki value hta deta h
+
+//console.log(myArr);
+
+//console.log((myArr.includes(9))); // it shows value is prsent or not in array
+// false or true me answer deta hai 
+
+//console.log(myArr.indexOf(3));
+// -1 aayega toh value nhi hai orr agr value hai toh uski position aayegi
+
+
+
+const newArr = myArr.join()  // array ko string me change kr deta hai
+
+//console.log(myArr);
+//normal array ki form me output milega
+
+//console.log(newArr);
+//array to string me convert ho jayega phr output milega
+
+//console.log(typeof newArr);
+
+
+
+
+
+// slice , splice
+
+
+const Animal = ["DOG","CAT","COW","DUCK","GOAT"]
+
+//console.log(Animal.slice(1,4)); // staring include krta hai ending array hta deta 
+
+
+//console.log(Animal.splice(1,4));  // staring and ending dono add krta h
+
+////////MOST IMPORTANT : SPLICE ORIGINAL ARRAY KO MANIPULATE KRTA HAI
+
+
+
+
+
+
+
+
+
+
+
