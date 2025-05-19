2014f1800fd3a9b11e9d02ee12884359ba35f3e9
DeepanshuXcode <deepanshusingh0708@gmail.com>
2025-05-19 00:03:09 +0000

    Math and num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,53 @@
+// //const score = 400 
+
+
+// const balance = new Number(500)
+// console.log(balance);
+
+// //console.log(balance.toString());
+
+// //console.log(typeof balance.toString);  // ???
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3)); //we use bcz when we want fixed value after decimal.
+
+const otherNumber = 34.7844
+//console.log(otherNumber.toPrecision(4)); //exact value lene ke liye use krte hai
+
+const hundreds = 19876543
+//console.log(hundreds.toLocaleString('en-IN'));
+//use for easily counting
+
+
+
+//******************** MATHS ********************//
+
+// console.log(Math);
+// console.log(Math.abs(-5));  //used for absoulate value e.g. -5 = 5
+
+// console.log(Math.round(6.8));  //used for round off value and mostly used e.g. 6.8 = 7 , 8.3 = 8
+//  console.log(Math.ceil(4.2));  
+//  console.log(Math.floor(4.2));  
+// console.log(Math.min(4,3,5,7,8,5,7));  //3
+// console.log(Math.max(4,3,5,7,9,6,5));   //9
+
+
+//console.log((Math.random()*10) + 1)
+
+const min = 10 
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1 )) + min)
+
+
+
+
+
+
+
+
+
+
+
+
+
